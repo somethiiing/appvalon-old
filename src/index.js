b3d5b7b6a0e181efa60bd48ea70ced3cee28d8ed
vinhvuu224 <vinh.vu@sjsu.edu>
2019-11-10 18:30:53 -0800

    revert index.js changes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import 'typeface-roboto';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ApiUI from './ApiUI';
 
-ReactDOM.render(<ApiUI />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
