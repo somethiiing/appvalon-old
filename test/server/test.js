dab82d5c94ccd2adc6a2140e299b031d963c47d5
somethiiing <hello@wilsonyu.io>
2019-11-24 17:21:28 -0800

    add test framework

---
+++ test/server/test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const expect = require('chai').expect;
+const should = require('chai').should();
+
+describe('test', () => {
+  it('test2', () => {
+    expect(true).to.be.true;
+  })
+})
